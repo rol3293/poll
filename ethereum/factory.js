f4f62632f7bf08b146cd762e72369bf58f0d5eb4
Hawad Ahmad <hawadjan.1@gmail.com>
2022-01-22 18:37:06 -0500

    new address

--- ethereum/factory.js
+++ ethereum/factory.js
@@ -1,6 +1,6 @@
 import web3 from "./web3";
 const { abi } = require("./build/PollFactory.json");
 
-const factory = new web3.eth.Contract(abi, '0x17B82dA35E3db3D1fF7116b2da9f5622E94Ac7c6');
+const factory = new web3.eth.Contract(abi, '0x5578d338836e669890dc33312a54792A3cf914b6');
 
 export default factory;
